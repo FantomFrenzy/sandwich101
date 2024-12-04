f0590846ff2fb1c9c6a8dcac6aa66b74fb36cf25
irelandallison <iealliso@ucsc.edu>
2024-12-03 17:27:40 -0800

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,10 +1,7 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Sandwich 101
+// Author: Group 14
+// Date: 12/09/24
 
-// Constants
-
-// Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
